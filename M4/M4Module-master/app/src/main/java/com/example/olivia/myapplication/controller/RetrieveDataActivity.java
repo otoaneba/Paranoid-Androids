ce97028ce489ee2ffb2a3ab462db132ddf842ffe
Kyung Jun Lee <klee702@gatech.edu>
2017-03-19 21:23:03 -0400

    implemented database

---
+++ M4/M4Module-master/app/src/main/java/com/example/olivia/myapplication/controller/RetrieveDataActivity.java
@@ -0,0 +1,131 @@
+package com.example.olivia.myapplication.controller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.example.olivia.myapplication.model.User;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class RetrieveDataActivity extends Activity {
+
+    String myJSON;
+    private static final String TAG_RESULTS="result";
+    private static final String TAG_USERNAME = "username";
+    private static final String TAG_NAME ="name";
+    private static final String TAG_PASS = "password";
+    private static final String TAG_EMAIL ="email";
+    private static final String TAG_ADD ="address";
+    private static final String TAG_TYPE = "userType";
+
+
+    JSONArray peoples = null;
+    public static ArrayList <User> users = new ArrayList<User>();
+    User user;
+    ArrayList<HashMap<String, String>> personList;
+
+    ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrieve_data);
+        list = (ListView) findViewById(R.id.listView);
+        personList = new ArrayList<HashMap<String,String>>();
+        getData("http://192.168.2.2:81/android_connect/example.php");
+    }
+
+
+    protected void showList(){
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for(int i=0;i<peoples.length();i++){
+                JSONObject c = peoples.getJSONObject(i);
+                String username = c.getString(TAG_USERNAME);
+                String name = c.getString(TAG_NAME);
+                String password = c.getString(TAG_PASS);
+                String email = c.getString(TAG_EMAIL);
+                String address = c.getString(TAG_ADD);
+                String userType = c.getString(TAG_TYPE);
+                HashMap<String,String> persons = new HashMap<String,String>();
+
+                persons.put(TAG_USERNAME,username);
+                persons.put(TAG_NAME,name);
+                persons.put(TAG_PASS,password);
+                persons.put(TAG_EMAIL,email);
+                persons.put(TAG_ADD,address);
+                persons.put(TAG_TYPE,userType);
+                personList.add(persons);
+                user = new User(username, name, password, email, address, userType);
+                users.add(user);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    RetrieveDataActivity.this, personList, R.layout.user_list,
+                    new String[]{TAG_USERNAME, TAG_NAME, TAG_EMAIL, TAG_PASS, TAG_ADD, TAG_TYPE},
+                    new int[]{R.id.username_list, R.id.name_list, R.id.email_list,
+                            R.id.password_list,  R.id.address_list, R.id.userType_list}
+            );
+
+            //list.setAdapter(adapter);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(getApplicationContext(),WelcomeActivity.class));
+    }
+
+    public void getData(String url){
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while((json = bufferedReader.readLine())!= null){
+                        sb.append(json+"\n");
+                    }
+
+                    return sb.toString().trim();
+
+                }catch(Exception e){
+                    return null;
+                }
+            }
+            @Override
+            protected void onPostExecute(String result){
+                myJSON=result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+    }
+
+}
+
--- M4/M4Module-master/app/src/main/java/com/example/olivia/myapplication/controller/SplashActivity.java
+++ M4/M4Module-master/app/src/main/java/com/example/olivia/myapplication/controller/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, WelcomeActivity.class);
+        Intent intent = new Intent(this, RetrieveDataActivity.class);
         startActivity(intent);
         finish();
     }
