1b0d281fed843687d9f0e272e8ae9ad1cb96dd4c
otoaneba <naotoabe1@gmail.com>
2017-03-27 19:27:13 -0400

    update createReportActivity

--- M4/M4Module-master/app/src/main/java/com/example/olivia/myapplication/controller/CreateReportActivity.java
+++ M4/M4Module-master/app/src/main/java/com/example/olivia/myapplication/controller/CreateReportActivity.java
@@ -38,134 +38,122 @@ public class CreateReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_create_report);
 
-        String address = "Address";
-        LatLng reportLatLng = new LatLng(-33.852, 151.211);
-        try {
-            Bundle extras = getIntent().getExtras();
+        String address = null;
+        LatLng updateLatLng = null;
 
-            String ifNull= extras.getString("address");
-            if (ifNull.length() != 0) {
-                address = ifNull;
-            }
-            Double latitude = extras.getDouble("latitude");
-            Double longitude = extras.getDouble("longitude");
-            reportLatLng = new LatLng(latitude, longitude);
-
-            String address = null;
-            LatLng updateLatLng = null;
-
-        try {
-            Bundle updateExtras = getIntent().getExtras();
-            updateLatLng = updateExtras.getParcelable("reportLatLng");
-            address = updateExtras.getString("address");
-        } catch (Exception e) {
-            Log.e("error", e.toString());
-        }
-        if (updateLatLng == null) {
             try {
-                Bundle extras = getIntent().getExtras();
-                String ifNull = extras.getString("address");
-                if (ifNull.length() != 0) {
-                    address = ifNull;
-                }
-                Double latitude = extras.getDouble("latitude");
-                Double longitude = extras.getDouble("longitude");
-                updateLatLng = new LatLng(latitude, longitude);
+                Bundle updateExtras = getIntent().getExtras();
+                updateLatLng = updateExtras.getParcelable("reportLatLng");
+                address = updateExtras.getString("address");
             } catch (Exception e) {
                 Log.e("error", e.toString());
             }
-        }
-
-        final LatLng reportLatLng1 = updateLatLng;
-        final String address1 = address;
-
-        user = (User) getIntent().getSerializableExtra("user"); //Obtaining data
-        //This is a PurityReportManager object that will store the new report
-        final PurityReportManager manager = new PurityReportManager();
-
-        //Initializes water conditions spinner
-        final Spinner etSpinner = (Spinner) findViewById(R.id.etConditionSpinner);
-        final ArrayAdapter<String> adapter2 = new ArrayAdapter(this,android.R.layout.simple_spinner_item, waterQuality.values());
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        etSpinner.setAdapter(adapter2);
-
-        final Button mapButton = (Button) findViewById(R.id.location_button);
-        //final EditText etTime = (EditText) findViewById(R.id.etTime);
-        final TextView etLocation = (TextView) findViewById(R.id.addressTV);
-        etLocation.setText(address);
-        final EditText etVirusPPM = (EditText) findViewById(R.id.etVirusPPM);
-        final EditText etContaminatePPM = (EditText) findViewById(R.id.etContaminatePPM);
-        final Button registerButton = (Button) findViewById(R.id.createButton);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Gets current time and date
-                Calendar c = new GregorianCalendar();
-                SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
-                SimpleDateFormat todayFormat = new SimpleDateFormat("MM/dd/yyyy");
-                final String todayDate = "" + todayFormat.format(c.getTime()).toString();
-
-                //Gets information from textboxes
-                final String time = "" + timeFormat.format(c.getTime()).toString();
-                final String location = "ad";
-                final String virusPPM = etVirusPPM.getText().toString();
-                final String contaminatePPM = etContaminatePPM.getText().toString();
-                final String condition = etSpinner.getSelectedItem().toString();
-                //Checks to see if there is a missing input
-                if (/*time.isEmpty() || */location.isEmpty() ||virusPPM.isEmpty() || contaminatePPM.isEmpty() ) {
-                    AlertDialog.Builder myAlert = new AlertDialog.Builder(CreateReportActivity.this);
-                    myAlert.setMessage("Time,location,virusPPM and comninationPPM required")
-                            .setPositiveButton("Back", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            })
-                            .create();
-                    myAlert.show();
-                }else if (etLocation.getText().toString().equals("Address")) {
-                    AlertDialog.Builder myAlert = new AlertDialog.Builder(CreateReportActivity.this);
-                    myAlert.setMessage("Click LOCATION button and set a location")
-                            .setPositiveButton("Back", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            })
-                            .create();
-                    myAlert.show();
+            if (updateLatLng == null) {
+                try {
+                    Bundle extras = getIntent().getExtras();
+                    String ifNull = extras.getString("address");
+                    if (ifNull.length() != 0) {
+                        address = ifNull;
+                    }
+                    Double latitude = extras.getDouble("latitude");
+                    Double longitude = extras.getDouble("longitude");
+                    updateLatLng = new LatLng(latitude, longitude);
+                } catch (Exception e) {
+                    Log.e("error", e.toString());
                 }
-                else {
-                    manager.addReport(time, address1, reportLatLng1, Double.parseDouble(virusPPM),
-                            Double.parseDouble(contaminatePPM), condition,
-                            manager.size() + 1, todayDate);
+            }
+
+            final LatLng reportLatLng1 = updateLatLng;
+            final String address1 = address;
+
+            user = (User) getIntent().getSerializableExtra("user"); //Obtaining data
+            //This is a PurityReportManager object that will store the new report
+            final PurityReportManager manager = new PurityReportManager();
+
+            //Initializes water conditions spinner
+            final Spinner etSpinner = (Spinner) findViewById(R.id.etConditionSpinner);
+            final ArrayAdapter<String> adapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, waterQuality.values());
+            adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            etSpinner.setAdapter(adapter2);
+
+            final Button mapButton = (Button) findViewById(R.id.location_button);
+            //final EditText etTime = (EditText) findViewById(R.id.etTime);
+            final TextView etLocation = (TextView) findViewById(R.id.addressTV);
+            etLocation.setText(address);
+            final EditText etVirusPPM = (EditText) findViewById(R.id.etVirusPPM);
+            final EditText etContaminatePPM = (EditText) findViewById(R.id.etContaminatePPM);
+            final Button registerButton = (Button) findViewById(R.id.createButton);
+            registerButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Gets current time and date
+                    Calendar c = new GregorianCalendar();
+                    SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
+                    SimpleDateFormat todayFormat = new SimpleDateFormat("MM/dd/yyyy");
+                    final String todayDate = "" + todayFormat.format(c.getTime()).toString();
+
+                    //Gets information from textboxes
+                    final String time = "" + timeFormat.format(c.getTime()).toString();
+                    final String location = "ad";
+                    final String virusPPM = etVirusPPM.getText().toString();
+                    final String contaminatePPM = etContaminatePPM.getText().toString();
+                    final String condition = etSpinner.getSelectedItem().toString();
+                    //Checks to see if there is a missing input
+                    if (/*time.isEmpty() || */location.isEmpty() || virusPPM.isEmpty() || contaminatePPM.isEmpty()) {
+                        AlertDialog.Builder myAlert = new AlertDialog.Builder(CreateReportActivity.this);
+                        myAlert.setMessage("Time,location,virusPPM and comninationPPM required")
+                                .setPositiveButton("Back", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                                .create();
+                        myAlert.show();
+                    } else if (etLocation.getText().toString().equals("Address")) {
+                        AlertDialog.Builder myAlert = new AlertDialog.Builder(CreateReportActivity.this);
+                        myAlert.setMessage("Click LOCATION button and set a location")
+                                .setPositiveButton("Back", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                                .create();
+                        myAlert.show();
+                    } else {
+                        manager.addReport(time, address1, reportLatLng1, Double.parseDouble(virusPPM),
+                                Double.parseDouble(contaminatePPM), condition,
+                                manager.size() + 1, todayDate);
+                        Intent intent = new Intent(CreateReportActivity.this, MainActivity.class);
+                        intent.putExtra("user", user);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            });
+            //cancel button that takes a user back to the welcome screen
+            final Button cancelButton = (Button) findViewById(R.id.cancelButton);
+            cancelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
                     Intent intent = new Intent(CreateReportActivity.this, MainActivity.class);
                     intent.putExtra("user", user);
                     startActivity(intent);
                     finish();
                 }
-            }
-        });
-        //cancel button that takes a user back to the welcome screen
-        final Button cancelButton = (Button) findViewById(R.id.cancelButton);
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CreateReportActivity.this, MainActivity.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-                finish();
-            }
-        });
+            });
 
-        mapButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CreateReportActivity.this, PickPurityReportsLocationActivity.class);
-                intent.putExtra("user", user);
-                startActivity(intent);
-                finish();
-            }
-        });
+            mapButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(CreateReportActivity.this, PickPurityReportsLocationActivity.class);
+                    intent.putExtra("user", user);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+        }
+
     }
-}
+
